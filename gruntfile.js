62ebeca735d38910f685e09a2d3f0697217819d1
Tom Clark <thrclark@iu.edu>
2017-08-25 13:51:28 -0400

    Formatting

--- gruntfile.js
+++ gruntfile.js
@@ -1,22 +1,22 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			dist: {
-				files: {
-					'css/admin.css' : 'sass/admin.scss',
-                    'css/main.css' : 'sass/main.scss'
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['sass']
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'css/admin.css': 'sass/admin.scss',
+                    'css/main.css': 'sass/main.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch']);
 }
