0873cba2529b51f4aa292c00db7e0c40c56bd90e
Tom Clark <thrclark@iu.edu>
2017-10-16 13:29:11 -0400

    New badge themed

--- php/includes/user-ads-myads.php
+++ php/includes/user-ads-myads.php
@@ -49,7 +49,7 @@
         </div>
     </div>
     <div class="col-12 text-center other-ads">
-        <div class="other-ads-content">My expired ads</div>
+        <div class="other-ads-content">My inactive ads</div>
         <hr>
     </div>
     <div class="ad2 grid cat-vehicles">
@@ -59,13 +59,13 @@
                     <div class="ad-image" style="background-image:url(../img/ad-pics/vehicles2.jpg)"> </div>
                 </div>
                 <div class="ad2-cont2">
-                    <div class="watchlist-set"> <i aria-hidden="true" class="fa fa-heart-o"></i> </div>
+                    <div class="watchlist-set" style="display:none"> <i aria-hidden="true" class="fa fa-heart-o"></i> </div>
                 </div>
-                <div class="ad2-cont4 ad-title" style="word-wrap: break-word;">Selling: Honda Accord 2001 (150 K miles) </div>
-                <div class="ad2-cont5 ad-price" style="word-wrap: break-word;">$2950</div>
+                <div class="ad2-cont4 ad-title" >Selling: Honda Accord 2001 (150 K miles) </div>
+                <div class="ad2-cont5 ad-price" >$2950</div>
                 <div class="ad2-cont5-1 ad-type"> <i title="Selling" class="fa fa-tag" aria-hidden="true"></i> </div>
                 <div class="w-100"></div>
-                <div class="ad2-cont6 ad-description" style="word-wrap: break-word;">Great student car which is in mechanically perfect condition. Sunroof, A/C &amp; Heater works perfectly. Regular check and maintenance done at Firestone. No mechanical issues, clean title and car fax report available on request. Very reliable and low maintenance. Shall only accept cash, please feel free to get in touch for test drive and inspection. </div>
+                <div class="ad2-cont6 ad-description" >Great student car which is in mechanically perfect condition. Sunroof, A/C &amp; Heater works perfectly. Regular check and maintenance done at Firestone. No mechanical issues, clean title and car fax report available on request. Very reliable and low maintenance. Shall only accept cash, please feel free to get in touch for test drive and inspection. </div>
                 <div class="ad2-cont7 ad-date">Oct 16</div>
                 <div class="ad2-cont8 ad-location"><i class="fa fa-map-marker" aria-hidden="true"></i> IUK</div>
             </div>
@@ -78,13 +78,13 @@
                     <div class="ad-image none"> </div>
                 </div>
                 <div class="ad2-cont2">
-                    <div class="watchlist-set"> <i aria-hidden="true" class="fa fa-heart-o"></i> </div>
+                   <div class="watchlist-set" style="display:none"> <i aria-hidden="true" class="fa fa-heart-o"></i> </div>
                 </div>
-                <div class="ad2-cont4 ad-title" style="word-wrap: break-word;">Selling: Storage Open !! </div>
-                <div class="ad2-cont5 ad-price" style="word-wrap: break-word;">$25/mo</div>
+                <div class="ad2-cont4 ad-title" >Selling: Storage Open !! </div>
+                <div class="ad2-cont5 ad-price" >$25/mo</div>
                 <div class="ad2-cont5-1 ad-type"> <i title="Selling" class="fa fa-tag" aria-hidden="true"></i> </div>
                 <div class="w-100"></div>
-                <div class="ad2-cont6 ad-description" style="word-wrap: break-word;">Are you looking for somewhere to store your luggage, funiture, bed, etc..? (for short or long term) 
+                <div class="ad2-cont6 ad-description" >Are you looking for somewhere to store your luggage, funiture, bed, etc..? (for short or long term) 
                     Extremely large, clean and safe room at my apartment.
                     (prices ; luggage ($25/mo) , funiture, etc - negotiable ) </div>
                 <div class="ad2-cont7 ad-date">Oct 16</div>
@@ -99,13 +99,13 @@
                     <div class="ad-image" style="background-image:url(../img/ad-pics/misc2.jpg)"> </div>
                 </div>
                 <div class="ad2-cont2">
-                    <div class="watchlist-set"> <i aria-hidden="true" class="fa fa-heart-o"></i> </div>
+                    <div class="watchlist-set" style="display:none"> <i aria-hidden="true" class="fa fa-heart-o"></i> </div>
                 </div>
-                <div class="ad2-cont4 ad-title" style="word-wrap: break-word;">Selling: 38 pieces Blue Indies Dish Set </div>
-                <div class="ad2-cont5 ad-price" style="word-wrap: break-word;">$50</div>
+                <div class="ad2-cont4 ad-title" >Selling: 38 pieces Blue Indies Dish Set </div>
+                <div class="ad2-cont5 ad-price" >$50</div>
                 <div class="ad2-cont5-1 ad-type"> <i title="Selling" class="fa fa-tag" aria-hidden="true"></i> </div>
                 <div class="w-100"></div>
-                <div class="ad2-cont6 ad-description" style="word-wrap: break-word;">Johnson Brothers Blue Indies Ironstone Dinnerware
+                <div class="ad2-cont6 ad-description" >Johnson Brothers Blue Indies Ironstone Dinnerware
                     Pattern in Cobalt Blue Flowers and Birds on white, subtle fluting
                     38 Pieces included: 
                     6 Dinner Plates (10" diameter);
