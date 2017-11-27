b8c25ceaf64c164bf2fdaed755db265430373310
Tom Clark <thrclark@iu.edu>
2017-11-27 12:19:58 -0500

    Index updates

--- php/user-components.php
+++ php/user-components.php
@@ -8,8 +8,9 @@ $page_title = 'cat_all';
 <?php include ('includes/head-meta.php') ?>
 <?php include('includes/user-styles.php') ?>
 <style>
-.counter {
-	
+h2 {
+	font-size: 18px;
+	margin-bottom:20px;
 }
 </style>
 </head>
@@ -17,20 +18,21 @@ $page_title = 'cat_all';
 <div id="main" >
     <section class="main-content" role="main">
         <div class="container">
+            <h1>Classifieds UI Components</h1>
+            <hr>
+            <h2 > Character Count Constraint</h2>
             <div class="row">
                 <div class="col-6">
-                    <form>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Text input (20 characters)</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="" maxlength="20">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleFormControlTextarea1">Textarea (30 characters)</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" maxlength="30"></textarea>
-                        </div>
-                    </form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Text input (20 characters)</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="" maxlength="20">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Textarea (30 characters)</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" maxlength="30"></textarea>
+                    </div>
                 </div>
-            </div>
+            </div><hr>
         </div>
     </section>
 </div>
