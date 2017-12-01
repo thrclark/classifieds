1007694662766cbd6d5944a39b8f860ae70361af
Tom Clark <thrclark@iu.edu>
2017-12-01 16:25:34 -0500

    Favorite component added throughout

--- php/includes/admin-footerscripts.php
+++ php/includes/admin-footerscripts.php
@@ -8,6 +8,7 @@
 <script src="../js/jscolor.js"></script>
 <script src="../js/jquery.sticky.js"></script>
 <script src="../js/bootstrap-maxlength.js"></script>
+
 <script>
   $(document).ready(function(){
     $(".app-header").sticky({topSpacing:0});
@@ -151,6 +152,7 @@ $(".closebtn").click(function(){
         });
     });
 </script>
+
 <script>
     $(document).ready(function() {
         $(".favorite").click(function() {
