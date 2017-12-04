30c1d3add7f2c7fe426908ab057a6aaccd341aa8
Tom Clark <thrclark@iu.edu>
2017-12-04 12:03:56 -0500

    Design propagation

--- php/modals/modal-main-ad-detail.php
+++ php/modals/modal-main-ad-detail.php
@@ -1,5 +1,61 @@
-<!-- Modal -->
-<style type="text/css"></style>
+<div class="modal fade" id="post_detail" tabindex="-1" role="dialog" aria-labelledby="post_detail" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title" id="exampleModal1Label">Modal title</h3>
+                <button type="button" class="modal-close" data-dismiss="modal" aria-label="Close" style=""> <span aria-hidden="true"> <i class="fa fa-close" aria-hidden="true"></i> </span> </button>
+            </div>
+            <div class="modal-body">
+               
+               
+               
+               
+               
+                               <div class="your-class">
+                    <div> <img src="../img/post-pics/1105499015MarkVITen1.jpg" class="img-fluid img-thumbnail " alt=""/></div>
+                    <div> <img src="../img/post-pics/1105499015MarkVITen2.jpg" class="img-fluid img-thumbnail " alt=""/></div>
+                    <div> <img src="../img/post-pics/1105499015MarkVITen3.jpg" class="img-fluid img-thumbnail " alt=""/></div>
+                    <div> <img src="../img/post-pics/1105499015MarkVITen4.jpg" class="img-fluid img-thumbnail " alt=""/></div>
+                    <div> <img src="../img/post-pics/1105499015MarkVITen5.jpg" class="img-fluid img-thumbnail " alt=""/></div>
+                    <div> <img src="../img/post-pics/1105499015MarkVITen6.jpg" class="img-fluid img-thumbnail " alt=""/></div>
+                    <div> <img src="../img/post-pics/1105499015MarkVITen7.jpg" class="img-fluid img-thumbnail " alt=""/></div>
+                    <div> <img src="../img/post-pics/1105499015MarkVITen8.jpg" class="img-fluid img-thumbnail " alt=""/></div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="ad-price">$11,225.00</div>
+                    </div>
+                    <div class="col align-self-end text-right">
+                        <div class=" ad-datelocation"><span class="ad-datelocation-date">Oct 31</span> | <span class="ad-datelocation-location"> <i class="fa fa-map-marker" aria-hidden="true"></i> IUK</span></div>
+                    </div>
+                </div>
+                <hr>
+                <dl class="row">
+                    <dt class="col-sm-12">Description</dt>
+                    <dd class="col-sm-12">This is a beautiful original lacquer Mark VI tenor saxophone from 1962, serial number 99015. The lacquer finish on the horn is in exquisite condition. Its original finish displays a very dark honey tint which is perfectly complemented by the crisp American floral engraving. The lacquer condition is about as clean and pristine as you will ever see.  </dd>
+                    <dt class="col-sm-12">Contact</dt>
+                    <dd class="col-sm-12"> 812-555-4567 </dd>
+                    <dt class="col-sm-12">Email</dt>
+                    <dd class="col-sm-12"><a href="mailto:thrclark@iu.edu">thrclark@iu.edu</a></dd>
+                    <dt class="col-sm-12">Actions</dt>
+                    <dd class="col-sm-12"> <a href="#" class="btn btn-outline-secondary btn-sm"><i class="fa fa-envelope-o" aria-hidden="true"></i> Share Ad</a> <button class="btn btn-outline-secondary btn-sm favorite"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="">Add to Watchlist</span></button> </dd>
+                </dl>
+                
+                
+                
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary">Action</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<!--
 
 <div class="modal fade" id="post_detail" tabindex="-1" role="dialog" aria-labelledby="post_detail" aria-hidden="true">
     <div class="modal-dialog post-detail" role="document">
@@ -45,3 +101,4 @@
         </div>
     </div>
 </div>
+-->
