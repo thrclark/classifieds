0a7b0f2bf1d1da0a453b0ad88a6ad885686ceb4a
Tom Clark <thrclark@iu.edu>
2017-12-11 17:20:15 -0500

    user options

---
+++ php/includes/main-user-options.php
@@ -0,0 +1,6 @@
+
+<ul class="nav flex-column user-options">
+    <li class="nav-item"> <a class="nav-link" href="admin-branding.php">Admin</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="main-myads.php">My Ads (2)</a> </li>
+    <li class="nav-item"> <a class="nav-link" href="main-watchlist.php">Watchlist (4) <i aria-hidden="true" class="fa fa-star pull-right"></i></a> </li>
+</ul>
