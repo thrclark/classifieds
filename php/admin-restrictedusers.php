200c135de4e1d324c1bf7ae10a472ffb58f491da
Tom Clark <thrclark@iu.edu>
2018-01-09 12:15:12 -0700

    work in progress

--- php/admin-restrictedusers.php
+++ php/admin-restrictedusers.php
@@ -32,7 +32,128 @@ $page_title = 'restrictedusers';
                         <div class="col doc-title">
                             <h2>Restricted Users</h2>
                         </div>
+                        <div class="col doc-controls"> 
+                            <!-- <button type="button" class="btn btn-outline-secondary btn-sm float-md-right">Restrict User</button>--> 
+                        </div>
                     </div>
+                </div>
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item"> <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Flagged Ads</a> </li>
+                    <li class="nav-item"> <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Warned & Restricted Users</a> </li>
+                 
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">                <table class="table table-sm table-actions">
+                    <thead>
+                        <tr>
+                            <th> Username</th>
+                            <th><strong>University Id</strong></th>
+                            <th>Status</th>
+                            <th><span class="sr-only">Actions</span></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>thrclark</td>
+                            <td>0001328543</td>
+                            <td>Flagged</td>
+                            <td><button type="button" class="btn btn-outline-secondary btn-sm">Action</button></td>
+                        </tr>
+                        <tr>
+                            <td>tewtband</td>
+                            <td>0003023985</td>
+                            <td>Flagged</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>zealfleo</td>
+                            <td>0003956434</td>
+                            <td>Flagged</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>languid</td>
+                            <td>0002534347</td>
+                            <td>Warned</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>accljones</td>
+                            <td>0004436419</td>
+                            <td>Flagged</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>sgdsteven</td>
+                            <td>0002635478</td>
+                            <td>Restricted</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>lerrome</td>
+                            <td>0002888753</td>
+                            <td>Warned</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                    </tbody>
+                </table>
+</div>
+                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">                <table class="table table-sm table-actions">
+                    <thead>
+                        <tr>
+                            <th> Username</th>
+                            <th><strong>University Id</strong></th>
+                            <th>Status</th>
+                            <th><span class="sr-only">Actions</span></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>thrclark</td>
+                            <td>0001328543</td>
+                            <td>Flagged</td>
+                            <td><button type="button" class="btn btn-outline-secondary btn-sm">Action</button></td>
+                        </tr>
+                        <tr>
+                            <td>tewtband</td>
+                            <td>0003023985</td>
+                            <td>Flagged</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>zealfleo</td>
+                            <td>0003956434</td>
+                            <td>Flagged</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>languid</td>
+                            <td>0002534347</td>
+                            <td>Warned</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>accljones</td>
+                            <td>0004436419</td>
+                            <td>Flagged</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>sgdsteven</td>
+                            <td>0002635478</td>
+                            <td>Restricted</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>lerrome</td>
+                            <td>0002888753</td>
+                            <td>Warned</td>
+                            <td>&nbsp;</td>
+                        </tr>
+                    </tbody>
+                </table>
+</div>
+                   
                 </div>
             </div>
         </div>
--- php/admin-userads.php
+++ php/admin-userads.php
@@ -30,20 +30,246 @@ $page_title = 'userads';
                 <div class="doc-header mb-3">
                     <div class="row">
                         <div class="col doc-title">
-                            <h2>User Ads</h2>
+                            <h2>Moderator</h2>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-6">
-                        <div class="form-group clear-field">
-                            <label class="sr-only" for="table_filter">Filter table</label>
-                            <input type="text" class="form-control" id="table_filter" placeholder="filter" >
-                            <div class="cleartext" style="display:none"> <i class="fa fa-window-close"></i></div>
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item"> <a class="nav-link active" id="allads-tab" data-toggle="tab" href="#allads" role="tab" aria-controls="allads" aria-selected="true">All Ads</a> </li>
+               
+                    <li class="nav-item"> <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Restricted Users</a> </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="allads" role="tabpanel" aria-labelledby="allads-tab">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="form-group clear-field">
+                                    <label class="sr-only" for="table_filter">Filter table</label>
+                                    <input type="text" class="form-control" id="table_filter" placeholder="filter" >
+                                    <div class="cleartext" style="display:none"> <i class="fa fa-window-close"></i></div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <table class="table table-sm table-actions" >
+                        <table class="table table-sm" >
+                            <thead>
+                                <tr>
+                                    <th>Ad Title</th>
+                                    <th>Posted by</th>
+                                    <th>Status</th>
+                                </tr>
+                            </thead>
+                            <tbody id="table_userads">
+                                <tr>
+                                    <td>Sublease for Brownstone Apartments for Spring Semester </td>
+                                    <td>tutorsmob</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Math-M211 and M212 Textbook "Calculus Single Variable Early Transcendentals" ISBN: 9781305748217 </td>
+                                    <td>skylardy</td>
+                                    <td>Flagged</td>
+                                </tr>
+                                <tr>
+                                    <td><i class="fa fa-chevron-down" aria-hidden="true"></i> Self Defense Boxing Gloves <br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+</td>
+                                    <td>diewillow</td>
+                                    <td>Inactive</td>
+                                </tr>
+                                <tr>
+                                    <td>Offering ride to Indy Dec 21 around 10:30am </td>
+                                    <td>glintgodly</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>2018 Spring sublease Jan-July the Fields room Waterchase </td>
+                                    <td>roadmasked</td>
+                                    <td>Removed</td>
+                                </tr>
+                                <tr>
+                                    <td>Business Law with Online Access Code (16th Edition) </td>
+                                    <td>backlisp</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Spring 2018 Sublease 7Ten Apartments</td>
+                                    <td>numbadze</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Essentials of Helth Information Management (3rd Edition) </td>
+                                    <td>testifytub</td>
+                                    <td>Inactive</td>
+                                </tr>
+                                <tr>
+                                    <td>2014 JEEP Grand Cherokee 4*4 Limited 5w miles 1st owner </td>
+                                    <td>velaribbon</td>
+                                    <td>Inactive</td>
+                                </tr>
+                                <tr>
+                                    <td>Spring Sublease Large Room with Walk in </td>
+                                    <td>reamcoals</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>NOW HIRING '18 BARTENDERS AND SERVERS </td>
+                                    <td>factbest</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Folding table and chairs </td>
+                                    <td>oinkskier</td>
+                                    <td>Inactive</td>
+                                </tr>
+                                <tr>
+                                    <td>RIDE TO/FROM CHI FRI 12/22 </td>
+                                    <td>killspine</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Need a ride from Indy to Bloomington on Dec 30 </td>
+                                    <td>jawplenty</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>One bedroom sublet in 8th street </td>
+                                    <td>mendwhinny</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Math-M211 and M212 Textbook "Calculus Single Variable Early Transcendentals" ISBN: 9781305748217 </td>
+                                    <td>sharinggym</td>
+                                    <td>Flagged</td>
+                                </tr>
+                                <tr>
+                                    <td>Self Defense Boxing Gloves </td>
+                                    <td>growlsicko</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Offering ride to Indy Dec 21 around 10:30am </td>
+                                    <td>bomblove</td>
+                                    <td>Removed</td>
+                                </tr>
+                                <tr>
+                                    <td>2018 Spring sublease Jan-July the Fields room Waterchase </td>
+                                    <td>backlisp</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Business Law with Online Access Code (16th Edition) </td>
+                                    <td>factbest</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Spring 2018 Sublease 7Ten Apartments</td>
+                                    <td>tutorsmob</td>
+                                    <td>Inactive</td>
+                                </tr>
+                                <tr>
+                                    <td>Essentials of Helth Information Management (3rd Edition) </td>
+                                    <td>skylardy</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>2014 JEEP Grand Cherokee 4*4 Limited 5w miles 1st owner </td>
+                                    <td>diewillow</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Spring Sublease Large Room with Walk in </td>
+                                    <td>glintgodly</td>
+                                    <td>Inactive</td>
+                                </tr>
+                                <tr>
+                                    <td>NOW HIRING '18 BARTENDERS AND SERVERS </td>
+                                    <td>roadmasked</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Folding table and chairs </td>
+                                    <td>backlisp</td>
+                                    <td>Inactive</td>
+                                </tr>
+                                <tr>
+                                    <td>RIDE TO/FROM CHI FRI 12/22 </td>
+                                    <td>numbadze</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Need a ride from Indy to Bloomington on Dec 30 </td>
+                                    <td>testifytub</td>
+                                    <td>Removed</td>
+                                </tr>
+                                <tr>
+                                    <td>One bedroom sublet in 8th street </td>
+                                    <td>factbest</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Math-M211 and M212 Textbook "Calculus Single Variable Early Transcendentals" ISBN: 9781305748217 </td>
+                                    <td>oinkskier</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Self Defense Boxing Gloves </td>
+                                    <td>killspine</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Offering ride to Indy Dec 21 around 10:30am </td>
+                                    <td>jawplenty</td>
+                                    <td>Inactive</td>
+                                </tr>
+                                <tr>
+                                    <td>2018 Spring sublease Jan-July the Fields room Waterchase </td>
+                                    <td>mendwhinny</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Business Law with Online Access Code (16th Edition) </td>
+                                    <td>sharinggym</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Spring 2018 Sublease 7Ten Apartments</td>
+                                    <td>growlsicko</td>
+                                    <td>Inactive</td>
+                                </tr>
+                                <tr>
+                                    <td>Essentials of Helth Information Management (3rd Edition) </td>
+                                    <td>bomblove</td>
+                                    <td>Inactive</td>
+                                </tr>
+                                <tr>
+                                    <td>2014 JEEP Grand Cherokee 4*4 Limited 5w miles 1st owner </td>
+                                    <td>diewillow</td>
+                                    <td>Inactive</td>
+                                </tr>
+                                <tr>
+                                    <td>Spring Sublease Large Room with Walk in </td>
+                                    <td>bushmildew</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>NOW HIRING '18 BARTENDERS AND SERVERS </td>
+                                    <td>rakeplank</td>
+                                    <td>Active</td>
+                                </tr>
+                                <tr>
+                                    <td>Folding table and chairs </td>
+                                    <td>velaribbon</td>
+                                    <td>Active</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        
+                        <!--  <table class="table table-sm table-actions" >
                     <thead>
                         <tr>
                             <th> Market</th>
@@ -417,7 +643,66 @@ $page_title = 'userads';
                             <td><button type="button" class="btn btn-outline-secondary btn-sm" data-toggle="modal" data-target="#pub_addetails">View</button></td>
                         </tr>
                     </tbody>
-                </table>
+                </table>--> 
+                    </div>
+               
+                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                        <table class="table table-sm table-actions">
+                            <thead>
+                                <tr>
+                                    <th> Username</th>
+                                    <th><strong>University Id</strong></th>
+                                    <th>Status</th>
+                                    <th><span class="sr-only">Actions</span></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>thrclark</td>
+                                    <td>0001328543</td>
+                                    <td>Flagged</td>
+                                    <td><button type="button" class="btn btn-outline-secondary btn-sm">Action</button></td>
+                                </tr>
+                                <tr>
+                                    <td>tewtband</td>
+                                    <td>0003023985</td>
+                                    <td>Flagged</td>
+                                    <td>&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td>zealfleo</td>
+                                    <td>0003956434</td>
+                                    <td>Flagged</td>
+                                    <td>&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td>languid</td>
+                                    <td>0002534347</td>
+                                    <td>Warned</td>
+                                    <td>&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td>accljones</td>
+                                    <td>0004436419</td>
+                                    <td>Flagged</td>
+                                    <td>&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td>sgdsteven</td>
+                                    <td>0002635478</td>
+                                    <td>Restricted</td>
+                                    <td>&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td>lerrome</td>
+                                    <td>0002888753</td>
+                                    <td>Warned</td>
+                                    <td>&nbsp;</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
                 <?php include('includes/all-pagination.php') ?>
             </div>
         </div>
