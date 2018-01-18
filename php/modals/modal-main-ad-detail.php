c96f566180db7b31c213bf71b37bcd58ed3d39af
Tom Clark <thrclark@iu.edu>
2018-01-17 22:07:28 -0500

    saved work

--- php/modals/modal-main-ad-detail.php
+++ php/modals/modal-main-ad-detail.php
@@ -6,8 +6,7 @@
                 <button type="button" class="modal-close" data-dismiss="modal" aria-label="Close" style=""> <span aria-hidden="true"> <i class="fa fa-close" aria-hidden="true"></i> </span> </button>
             </div>
             <div class="modal-body post-details-body">
-            
-             <div class="row">
+                <div class="row">
                     <div class="col">
                         <div class="post-price">$11,225.00</div>
                     </div>
@@ -26,7 +25,6 @@
                     <div> <img src="../img/post-pics/1105499015MarkVITen7.jpg" class="img-fluid img-thumbnail " alt=""/></div>
                     <div> <img src="../img/post-pics/1105499015MarkVITen8.jpg" class="img-fluid img-thumbnail " alt=""/></div>
                 </div>
-               
                 <dl class="row">
                     <dt class="col-sm-12">Description</dt>
                     <dd class="col-sm-12">This is a beautiful original lacquer Mark VI tenor saxophone from 1962, serial number 99015. The lacquer finish on the horn is in exquisite condition. Its original finish displays a very dark honey tint which is perfectly complemented by the crisp American floral engraving. The lacquer condition is about as clean and pristine as you will ever see. </dd>
@@ -34,15 +32,15 @@
                     <dd class="col-sm-12"> 812-555-4567 </dd>
                     <dt class="col-sm-12">Email</dt>
                     <dd class="col-sm-12"><a href="mailto:thrclark@iu.edu">thrclark@iu.edu</a></dd>
-                    <dt class="col-sm-12">Actions</dt>
-                    <dd class="col-sm-12"> <a href="#" class="btn btn-outline-secondary btn-sm"><i class="fa fa-envelope-o" aria-hidden="true"></i> Share Ad</a>
-                        <button class="btn btn-outline-secondary btn-sm favorite"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="">Add to Watchlist</span></button>
-                    </dd>
                 </dl>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-link" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-primary">Action</button>
+                
+                  <button type="button" class="btn btn-sm btn-primary" >Report This</button>
+                <button type="button" class="btn btn-sm btn-primary">Share Ad</button>
+                <button type="button" class="btn btn-sm btn-primary favorite"> <i aria-hidden="true" class="fa fa-star-o"></i> Watchlist</button>
+                <button type="button" class="btn btn-sm btn-primary">Edit Ad</button>
+                <button type="button" class="btn btn-sm btn-primary">Delete Ad</button>
             </div>
         </div>
     </div>
