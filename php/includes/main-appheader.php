efe681be926372f84e12a41f3d431da54101152c
Tom Clark <thrclark@iu.edu>
2018-01-30 18:30:37 -0500

    synced - main app header

--- php/includes/main-appheader.php
+++ php/includes/main-appheader.php
@@ -1,4 +1,54 @@
 
+
+<div class="app-header" id="mainHeader">
+	<div class="container">
+		<div class="row align-items-center justify-content-between">
+			<div class="col-auto brand"> <!--this used to be just 'col' ?-->
+				<div class="form-row align-items-center">
+                    <!----><div class="col-auto">
+                        <div class="brand-image"> <img src="../img/trident-large.png" alt="Indiana University" /></div>
+                    </div>
+                    <div class="col-auto">
+                        <!---->
+                        <!----><h1 class="brand-text">Classifieds</h1>
+                    </div>
+                </div>
+			</div>
+			<div class="col search d-none d-md-block">
+				<div class="search-container">
+					<div class="row align-items-center">
+						<div class="col">
+							<label class="sr-only" for="mainsearch"><message key="global.button.search">Search</message></label>
+							<input class="form-control search-input ng-pristine ng-valid ng-touched" id="mainsearch" type="text">
+						</div>
+						<div class="col-auto search-button text-right">
+							<button class="btn">
+								<i aria-hidden="true" class="fa fa-search"></i>
+								<span class="search-label d-none d-lg-inline"><message key="global.button.search">Search</message></span>
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-auto post d-none d-md-block">
+				<a class="btn toggle_mobilepost" routerlink="/posting" href="/classifieds/posting"><message key="global.header.button.postAd">Post Ad</message></a>
+			</div>
+			<div class="col-auto user-ads d-md-none text-right">
+				<button class="menu-trigger" type="button">
+					<i aria-hidden="true" class="fa fa-ellipsis-v"></i>
+					<i class="sr-only"><message key="global.button.openNavigation">Open User Menu</message></i>
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+	
+    
+<!--
+
+<br><br><br>
+
 <div class="app-header mb-3" role="banner">
     <div class="container">
         <div class="row align-items-center justify-content-between">
@@ -33,3 +83,4 @@
         </div>
     </div>
 </div>
+-->
