fcf340362a3aae72b95ab2d89b2cafd478012b6f
Tom Clark <thrclark@iu.edu>
2018-02-02 14:25:13 -0500

    saved work

--- php/includes/main-appheader.php
+++ php/includes/main-appheader.php
@@ -1,50 +1,76 @@
 
-
-<div class="app-header" id="mainHeader">
-	<div class="container">
-		<div class="row align-items-center justify-content-between">
-			<div class="col-auto brand"> <!--this used to be just 'col' ?-->
-				<div class="form-row align-items-center">
-                    <!----><div class="col-auto">
-                        <div class="brand-image"> <img src="../img/trident-large.png" alt="Indiana University" /></div>
-                    </div>
-                    <div class="col-auto">
-                        <!---->
-                        <!----><h1 class="brand-text">Classifieds</h1>
+<header class="rbt-app-header rbt-app-header--light">
+    <div class="container">
+        <div class="row align-items-center"> 
+            
+            <!--App Logo Image-->
+            <div class="col-auto rbt-brand-image-container"> <img src="../img/trident-large.png" alt="trident" class="rbt-brand-image"/> </div>
+            
+            <!--App Title-->
+            <div class="col col-lg-auto rbt-brand-title-container">
+                <h1 class="rbt-brand-title">Classifieds </h1>
+            </div>
+            <div class="col">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="button">Button</button>
                     </div>
                 </div>
-			</div>
-			<div class="col search d-none d-md-block">
-				<div class="search-container">
-					<div class="row align-items-center">
-						<div class="col">
-							<label class="sr-only" for="mainsearch"><message key="global.button.search">Search</message></label>
-							<input class="form-control search-input ng-pristine ng-valid ng-touched" id="mainsearch" type="text">
-						</div>
-						<div class="col-auto search-button text-right">
-							<button class="btn" onclick="location.href = 'main-searchresults.php';">
-								<i aria-hidden="true" class="fa fa-search"></i>
-								<span class="search-label d-none d-lg-inline"><message key="global.button.search">Search</message></span>
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-auto post d-none d-md-block">
-				<a class="btn toggle_mobilepost" routerlink="/posting" href="main-postad.php"><message key="global.header.button.postAd">Post Ad</message></a>
-			</div>
-			<div class="col-auto user-ads d-md-none text-right">
-				<button class="menu-trigger" type="button">
-					<i aria-hidden="true" class="fa fa-ellipsis-v"></i>
-					<i class="sr-only"><message key="global.button.openNavigation">Open User Menu</message></i>
-				</button>
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
+</header>
+<br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br>
+<br>
+<br>
+<div class="app-header" id="mainHeader">
+    <div class="container">
+        <div class="row align-items-center justify-content-between">
+            <div class="col-auto brand"> <!--this used to be just 'col' ?-->
+                <div class="form-row align-items-center"> 
+                    <!---->
+                    <div class="col-auto">
+                        <div class="brand-image"> <img src="../img/trident-large.png" alt="Indiana University" /></div>
+                    </div>
+                    <div class="col-auto"> 
+                        <!----> 
+                        <!---->
+                        <h1 class="brand-text">Classifieds</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col search d-none d-md-block">
+                <div class="search-container">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <label class="sr-only" for="mainsearch">
+                                <message key="global.button.search">Search</message>
+                            </label>
+                            <input class="form-control search-input ng-pristine ng-valid ng-touched" id="mainsearch" type="text">
+                        </div>
+                        <div class="col-auto search-button text-right">
+                            <button class="btn" onclick="location.href = 'main-searchresults.php';"> <i aria-hidden="true" class="fa fa-search"></i> <span class="search-label d-none d-lg-inline">
+                            <message key="global.button.search">Search</message>
+                            </span> </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-auto post d-none d-md-block"> <a class="btn toggle_mobilepost" routerlink="/posting" href="main-postad.php">
+                <message key="global.header.button.postAd">Post Ad</message>
+                </a> </div>
+            <div class="col-auto user-ads d-md-none text-right">
+                <button class="menu-trigger" type="button"> <i aria-hidden="true" class="fa fa-ellipsis-v"></i> <i class="sr-only">
+                <message key="global.button.openNavigation">Open User Menu</message>
+                </i> </button>
+            </div>
+        </div>
+    </div>
 </div>
 
-	
-    
 <!--
 
 <br><br><br>
