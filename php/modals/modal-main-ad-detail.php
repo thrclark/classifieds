aa8e7ff75f8f1c9beeb06e9956b60ce721ba83a7
Tom Clark <thrclark@iu.edu>
2018-02-26 09:30:42 -0500

    formatting

--- php/modals/modal-main-ad-detail.php
+++ php/modals/modal-main-ad-detail.php
@@ -15,23 +15,21 @@
                     </div>
                     <div class="col-auto small"> <i class="fa fa-map-marker"></i> IU Kokomo </div>
                 </div>
-              
-              
-                    <ccf-image-slider ng-reflect-image-urls="/ccf2-unt/postings/images/1800" >
-                        <div class="overflow mb-3" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
-                            <div class="inner" ng-reflect-ng-style="[object Object]" style="width: 400%; margin-left: 0%;"> <img ng-reflect-ng-style="[object Object]" src="../img/post-pics/1105499015MarkVITen5.jpg" style="width: 25%;"> </div>
-                        </div>
-                        <div class="controls">
-                            <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-left"></i> <span class="sr-only">
-                            <message key="main.imageSlider.prev" ng-reflect-key="main.imageSlider.prev">Previous image</message>
-                            </span> </button>
-                            <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-right"></i> <span class="sr-only">
-                            <message key="main.imageSlider.next" ng-reflect-key="main.imageSlider.next">Next image</message>
-                            </span> </button>
-                        </div>
-                    </ccf-image-slider>
-                    <!--<div class="post-descrip">asfd asdf asfd asfd asfd asf asdfasdf asfd asdf asdf asdf asdf </div>--> 
-               
+                <ccf-image-slider ng-reflect-image-urls="/ccf2-unt/postings/images/1800" >
+                    <div class="overflow mb-3" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                        <div class="inner" ng-reflect-ng-style="[object Object]" style="width: 400%; margin-left: 0%;"> <img ng-reflect-ng-style="[object Object]" src="../img/post-pics/1105499015MarkVITen5.jpg" style="width: 25%;"> </div>
+                    </div>
+                    <div class="controls">
+                        <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-left"></i> <span class="sr-only">
+                        <message key="main.imageSlider.prev" ng-reflect-key="main.imageSlider.prev">Previous image</message>
+                        </span> </button>
+                        <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-right"></i> <span class="sr-only">
+                        <message key="main.imageSlider.next" ng-reflect-key="main.imageSlider.next">Next image</message>
+                        </span> </button>
+                    </div>
+                </ccf-image-slider>
+                <!--<div class="post-descrip">asfd asdf asfd asfd asfd asf asdfasdf asfd asdf asdf asdf asdf </div>-->
+                
                 <dl>
                     <dt>Description</dt>
                     <dd>This is a beautiful original lacquer Mark VI tenor saxophone from 1962, serial number 99015. The lacquer finish on the horn is in exquisite condition. Its original finish displays a very dark honey tint which is perfectly complemented by the crisp American floral engraving. The lacquer condition is about as clean and pristine as you will ever see. </dd>
