f84a3950c2120824e5f66fa9704a50cb07fdda73
Tom Clark <thrclark@iu.edu>
2018-02-26 09:27:50 -0500

    ad detail - main

--- php/modals/modal-main-ad-detail.php
+++ php/modals/modal-main-ad-detail.php
@@ -3,50 +3,55 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h3 class="modal-title" id="exampleModalLabel">Ad details</h3>
+                <h3 class="modal-title" id="exampleModalLabel">For sale: Selmer VI Tenor Sax</h3>
                 <button type="button" class="close" aria-label="Close" data-dismiss="modal"> <span class="rbt-icon-close"></span> </button>
             </div>
-            <article>
-                <div class="modal-body">
-                    <div class="row">
-                        <div class="col-12">
-                            <h2 class="modal-title" id="exampleModalLabel">For sale: Selmer VI Tenor Sax</h2>
+            <div class="modal-body">
+                <div class="row no-gutters mb-3">
+                    <div class="col-12 rbt-ts-26">$1400.00 or best offer</div>
+                    <div class="col-auto small"> <span class="font-italic">2/14/18 </span> </div>
+                    <div class="col-auto small">
+                        <div class="mx-2 text-muted">|</div>
+                    </div>
+                    <div class="col-auto small"> <i class="fa fa-map-marker"></i> IU Kokomo </div>
+                </div>
+              
+              
+                    <ccf-image-slider ng-reflect-image-urls="/ccf2-unt/postings/images/1800" >
+                        <div class="overflow mb-3" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                            <div class="inner" ng-reflect-ng-style="[object Object]" style="width: 400%; margin-left: 0%;"> <img ng-reflect-ng-style="[object Object]" src="../img/post-pics/1105499015MarkVITen5.jpg" style="width: 25%;"> </div>
                         </div>
-                        <div class="col">$1400.00</div>
-                        <div class="col text-right"> <span class="font-italic">2/14/18</span> | IUK </div>
-                    </div>
-                    <hr>
-                    <div class="mb-3">
-                        <ccf-image-slider ng-reflect-image-urls="/ccf2-unt/postings/images/1800">
-                            <div class="overflow" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
-                                <div class="inner" ng-reflect-ng-style="[object Object]" style="width: 400%; margin-left: 0%;"> <img ng-reflect-ng-style="[object Object]" src="../img/post-pics/1105499015MarkVITen5.jpg" style="width: 25%;"> </div>
+                        <div class="controls">
+                            <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-left"></i> <span class="sr-only">
+                            <message key="main.imageSlider.prev" ng-reflect-key="main.imageSlider.prev">Previous image</message>
+                            </span> </button>
+                            <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-right"></i> <span class="sr-only">
+                            <message key="main.imageSlider.next" ng-reflect-key="main.imageSlider.next">Next image</message>
+                            </span> </button>
+                        </div>
+                    </ccf-image-slider>
+                    <!--<div class="post-descrip">asfd asdf asfd asfd asfd asf asdfasdf asfd asdf asdf asdf asdf </div>--> 
+               
+                <dl>
+                    <dt>Description</dt>
+                    <dd>This is a beautiful original lacquer Mark VI tenor saxophone from 1962, serial number 99015. The lacquer finish on the horn is in exquisite condition. Its original finish displays a very dark honey tint which is perfectly complemented by the crisp American floral engraving. The lacquer condition is about as clean and pristine as you will ever see. </dd>
+                    <dt>Contact</dt>
+                    <dd>812-555-4567</dd>
+                    <dt>Email</dt>
+                    <dd><a href="mailto:thrclark@iu.edu">thrclark@iu.edu</a></dd>
+                </dl>
+            </div>
+            <div class="modal-footer">
+                <div class="container-fluid px-0">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <div class="rbt-button-group">
+                                <button type="button" class="btn btn-outline-primary">Share</button>
+                                <button class="btn btn-outline-primary favorite"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="">Watchlist</span></button>
                             </div>
-                            <div class="controls">
-                                <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-left"></i> <span class="sr-only">
-                                <message key="main.imageSlider.prev" ng-reflect-key="main.imageSlider.prev">Previous image</message>
-                                </span> </button>
-                                <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-right"></i> <span class="sr-only">
-                                <message key="main.imageSlider.next" ng-reflect-key="main.imageSlider.next">Next image</message>
-                                </span> </button>
-                            </div>
-                        </ccf-image-slider>
-                        <!--<div class="post-descrip">asfd asdf asfd asfd asfd asf asdfasdf asfd asdf asdf asdf asdf </div>--> 
+                        </div>
+                        <div class="col-auto"> <a href="#" class="font-italic small">Report this</a> </div>
                     </div>
-                    <dl>
-                        <dt>Description</dt>
-                        <dd>This is a beautiful original lacquer Mark VI tenor saxophone from 1962, serial number 99015. The lacquer finish on the horn is in exquisite condition. Its original finish displays a very dark honey tint which is perfectly complemented by the crisp American floral engraving. The lacquer condition is about as clean and pristine as you will ever see. </dd>
-                        <dt>Contact</dt>
-                        <dd>812-555-4567</dd>
-                        <dt>Email</dt>
-                        <dd><a href="mailto:thrclark@iu.edu">thrclark@iu.edu</a></dd>
-                    </dl>
-                 </div>
-            </article>
-         <div class="modal-footer">
-                <div class="rbt-button-group">
-                    <button type="button" class="btn btn-outline-primary">Share</button>
-                    
-                    <button class="btn btn-outline-primary favorite"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="">Watchlist</span></button>
                 </div>
             </div>
         </div>
