92c55d07b32b35e5dce41146a0f08e8abb7e39c3
Tom Clark <thrclark@iu.edu>
2018-04-04 12:00:15 -0400

    CCF-487

--- php/includes/admin-footerscripts.php
+++ php/includes/admin-footerscripts.php
@@ -1,10 +1,6 @@
-	<script>
-    window.jQuery || document.write('<script src="../bower_components/jquery/dist/jquery.min.js"><\/script>')
-</script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
-<script src="../bower_components/filament-tablesaw/dist/tablesaw.jquery.js"></script>
-<script src="../bower_components/filament-tablesaw/dist/tablesaw-init.js"></script>
 <script src="../js/jquery.dotdotdot.min.js"></script>
 <script src="../js/slick/slick.min.js"></script>
 <script src="../js/jscolor.js"></script>
