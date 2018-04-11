bc355398fea5551037448e5d82d6c249ca59a6d9
Tom Clark <thrclark@iu.edu>
2018-04-11 08:37:53 -0400

    CCF-447

--- php/includes/admin-footerscripts.php
+++ php/includes/admin-footerscripts.php
@@ -1,4 +1,4 @@
-<script src="../node_modules/jquery/dist/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
 <script src="../js/jquery.dotdotdot.min.js"></script>
