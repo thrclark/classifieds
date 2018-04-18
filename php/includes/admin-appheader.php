174c39eeb56b7741b63dc98f07cee3e07d572f47
Tom Clark <thrclark@iu.edu>
2018-04-18 09:33:14 -0400

    brand1

--- php/includes/admin-appheader.php
+++ php/includes/admin-appheader.php
@@ -6,13 +6,13 @@
             
             <!--App Logo Image-->
             <div class="col-auto rbt-brand-image-container">
-                <img src="../img/trident-large.png" alt="" class="rbt-brand-image d-none d-sm-block"/><img src="../img/trident-large.png" alt="" class="rbt-brand-image d-sm-none"/>
+                <img src="../img/ccf-logo.png" alt="" class="rbt-brand-image d-none d-sm-block"/><img src="../img/ccf-logo-mobile.png" alt="" class="rbt-brand-image d-sm-none"/>
             </div>
             
             <!--App Title-->
             <div class="col rbt-brand-title-container">	
                 <span class="rbt-header-title">
-                <a  href="main-home.php"class="rbt-brand-title"> <strong>Classifieds</strong> <span class="rbt-ts-14">Administration</span></a>
+                <a  href="main-home.php"class="rbt-brand-title"> <!--<strong>Classifieds</strong>--> <span class="rbt-ts-14">Administration</span></a>
                 </span>
             </div>
             
