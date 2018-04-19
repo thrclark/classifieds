c599856b5380afb87a27edfb8d1b0bd225f4ee43
eecox <eecox@iu.edu>
2018-04-19 13:57:17 -0400

    corrected css

--- php/includes/main-styles.php
+++ php/includes/main-styles.php
@@ -13,4 +13,34 @@
 	margin-right: auto;
 	vertical-align: middle;
 }
+.line-it-up{
+	float: left;
+	padding-right: 10px;
+}
+.rbt-right-group {
+    margin-right: 15px;
+}
+.rbt-btn-progress {
+    -webkit-animation: progress-bar-stripes 1s linear infinite;
+    animation: progress-bar-stripes 1s linear infinite;
+    background-image: linear-gradient(45deg, rgba(255, 255, 255, 0.15) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, 0.15) 50%, rgba(255, 255, 255, 0.15) 75%, transparent 75%, transparent);
+    background-size: 1rem 1rem;
+}
+#loading{
+	width: 60px;
+	position: absolute;
+	z-index: 1;
+	left: 30px;
+	bottom: 15px;
+}
+@media(max-width:564px) {
+	.mod-actions{
+		padding: .3rem .3rem;
+		margin-right: .25rem !important;
+    	font-size: 78% !important;
+	}
+	.favorite .fa.fa-star-o {
+    	font-size: 14px;
+	}
+}
 </style>
