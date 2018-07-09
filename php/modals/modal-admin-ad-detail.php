0fd0833477d34048239f39f40f259ecc0aca52f0
Tom Clark <thrclark@iu.edu>
2018-07-09 09:38:44 -0400

    CCF-683

--- php/modals/modal-admin-ad-detail.php
+++ php/modals/modal-admin-ad-detail.php
@@ -1,59 +1,46 @@
 <div class="modal post-detail fade" id="admin_ad_detail" tabindex="-1" role="dialog" aria-labelledby="post_detail" aria-hidden="true">
 <div class="modal-dialog" role="document">
-
-
-
-
-
- <div class="modal-content">
-            <div class="modal-header">
-                <h3 class="modal-title" id="exampleModalLabel">For sale: Selmer VI Tenor Sax</h3>
-                <button type="button" class="close" aria-label="Close" data-dismiss="modal"> <span class="rbt-icon-close"></span> </button>
-            </div>
-            <div class="modal-body">
-            
-            
-            
-            
-            
-
-<div class="alert alert-danger fade show" role="alert">
-    <div class="alert-heading">Reported</div>
-    <p class="small mb-3">This ad has been reported by user 'thrclark' for potential policy violations.</p>
-    
-    
-  <p> <button class="btn btn-sm btn-outline-primary"> View report</button></p>
-    
-</div>
-
-
-
-
-                <div class="row no-gutters mb-3">
-                    <div class="col-12 rbt-ts-23">$1400.00 or best offer</div>
-                    <div class="col-auto small"> <span class="font-italic">2/14/18 </span> </div>
-                    <div class="col-auto small">
-                        <div class="mx-2 text-muted">|</div>
+    <div class="modal-content">
+        <div class="modal-header">
+            <h3 class="modal-title" id="exampleModalLabel">For sale: Selmer VI Tenor Sax</h3>
+            <button type="button" class="close" aria-label="Close" data-dismiss="modal"> <span class="rbt-icon-close"></span> </button>
+        </div>
+        <div class="modal-body" id="reported-content">
+            <div class="alert alert-danger fade show mt-3" role="alert">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <div class="alert-heading">Reported for policy violation</div>
                     </div>
-                    <div class="col-auto small"> <i class="fa fa-map-marker"></i> IU Kokomo </div>
+                    <div class="col-auto">
+                        <button class="btn btn-sm btn-outline-primary"> View report</button>
+                    </div>
                 </div>
-                <ccf-image-slider ng-reflect-image-urls="/ccf2-unt/postings/images/1800" >
-                    <div class="overflow mb-3" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
-                        <div class="inner" ng-reflect-ng-style="[object Object]" style="width: 400%; margin-left: 0%;"> <img ng-reflect-ng-style="[object Object]" src="../img/post-pics/1105499015MarkVITen5.jpg" style="width: 25%;"> </div>
-                    </div>
-                    <div class="controls">
-                        <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-left"></i> <span class="sr-only">
-                        <message key="main.imageSlider.prev" ng-reflect-key="main.imageSlider.prev">Previous image</message>
-                        </span> </button>
-                        <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-right"></i> <span class="sr-only">
-                        <message key="main.imageSlider.next" ng-reflect-key="main.imageSlider.next">Next image</message>
-                        </span> </button>
-                    </div>
-                </ccf-image-slider>
-                <!--<div class="post-descrip">asfd asdf asfd asfd asfd asf asdfasdf asfd asdf asdf asdf asdf </div>-->
-                
-                <dl>
-                     <dt>Description</dt>
+            </div>
+            <div class="row no-gutters mb-3">
+                <div class="col-12 rbt-ts-23">$1400.00 or best offer</div>
+                <div class="col-auto small"> <span class="font-italic">2/14/18 </span> </div>
+                <div class="col-auto small">
+                    <div class="mx-2 text-muted">|</div>
+                </div>
+                <div class="col-auto small"> <i class="fa fa-map-marker"></i> IU Kokomo </div>
+            </div>
+            <ccf-image-slider ng-reflect-image-urls="/ccf2-unt/postings/images/1800" >
+                <div class="overflow mb-3" style="touch-action: none; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                    <div class="inner" ng-reflect-ng-style="[object Object]" style="width: 400%; margin-left: 0%;"> <img ng-reflect-ng-style="[object Object]" src="../img/post-pics/1105499015MarkVITen5.jpg" style="width: 25%;"> </div>
+                </div>
+                <div class="controls">
+                    <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-left"></i> <span class="sr-only">
+                    <message key="main.imageSlider.prev" ng-reflect-key="main.imageSlider.prev">Previous image</message>
+                    </span> </button>
+                    <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-right"></i> <span class="sr-only">
+                    <message key="main.imageSlider.next" ng-reflect-key="main.imageSlider.next">Next image</message>
+                    </span> </button>
+                </div>
+            </ccf-image-slider>
+            <!--<div class="post-descrip">asfd asdf asfd asfd asfd asf asdfasdf asfd asdf asdf asdf asdf </div>-->
+            
+            <dl>
+                <dt>Description</dt>
                 <dd>This is a beautiful original lacquer Mark VI tenor saxophone from 1962, serial number 99015. The lacquer finish on the horn is in exquisite condition. Its original finish displays a very dark honey tint which is perfectly complemented by the crisp American floral engraving. The lacquer condition is about as clean and pristine as you will ever see. </dd>
                 <dt>Contact</dt>
                 <dd>812-555-4567</dd>
@@ -67,25 +54,81 @@
                 <dd>02/25/2018 3:26pm</dd>
                 <dt>Expiration Date</dt>
                 <dd>03/25/2018 3:26pm</dd>
-                </dl>
-            </div>
-            <div class="modal-footer">
-                <div class="container-fluid px-0">
-                    <div class="row align-items-center">
-                        <div class="col">
-                            <div class="rbt-button-group">
-                                  <button class="btn btn-outline-primary"> <span class="">Remove ad</span></button>
+            </dl>
+        </div>
+        <div class="modal-footer" id="reported-footer">
+            <div class="container-fluid px-0">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <div class="rbt-button-group">
+                            <div class="dropdown">
+                                <button class="btn btn-outline-primary dropdown-toggle" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Deactivate </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink"> <a class="dropdown-item" href="#">Ad</a> <a class="dropdown-item" href="#">Report</a> </div>
                             </div>
+                            <button class="btn btn-outline-primary"> <span class="">View report</span></button>
                         </div>
-                       
                     </div>
                 </div>
             </div>
         </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="modal-body demojs-viewreport" style="">
+            <div class="ccf-postdetail-title">Report details</div>
+           <p> <dl>
+                <dt>Reported by</dt>
+                <dd>thrclark</dd>
+                <dt>Reason for report</dt>
+                <dd>Promotes a political/social agenda</dd>
+                <dt>Additional detail</dt>
+                <dd>This ad is really a campaign ad for a candidate who is running for state senate. Please remove it.</dd>
+            </dl></p>
+            <p> <a href="#">View policy</a></p>
+        </div>
+        <div class="modal-footer demojs-viewreport">
+            <div class="container-fluid px-0">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <div class="rbt-button-group">
+                            <div class="dropdown">
+                                <button class="btn btn-outline-primary dropdown-toggle" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Deactivate </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink"> <a class="dropdown-item" href="#">Ad</a> <a class="dropdown-item" href="#">Report</a> </div>
+                            </div>
+                            <button class="btn btn-outline-primary"> <span class="">View ad</span></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="modal-body demojs-view-policy" style="">
+            <div class="ccf-postdetail-title">About this service</div>
+            <p>The IU Classifieds contain items of interest to the Indiana University community. Please remember that the use of the University's computer network for these messages is a privilege extended to students and employees for personal, non-commercial use. University resources are not allowed to be used for the promotion of commercial or personal businesses. In addition, the use of IU Classifieds to promote or provide pornographic materials or sexual services is strictly prohibited.  The buying or selling of IU parking permits is prohibited as parking permits are the property of the university. The terms of any transactions are between the buyer and seller. Classifieds may contain office telephone numbers; however, it is the shared responsibility of supervisors and employees to assure that this privilege is not abused. Honor all copyrights when posting images or text in the Classifieds.</p>
+            <p>Any housing postings must follow Federal and State laws. Please read <a href="http://www.craigslist.org/about/FHA" target="_blank">Fair Housing laws.</a> </p>
+            <p>By using this site, you agree to abide by Indiana Universitys "Appropriate Technology Use" Policies as outlined by the Information Policy Office at <a href="http://policies.iu.edu/policies/categories/information-it/it/IT-01.shtml" target="_blank">http://policies.iu.edu/policies/categories/information-it/it/IT-01.shtml</a> and to use the Classifieds appropriately. These pages are not intended for solicitation, political purposes and publishing opinions, nor are they to be used to make jokes or mislead others. The University reserves the right to remove any posting that it deems inappropriate.</p>
+            <p>Questions regarding the IU Classifieds may be addressed to <a href="mailto:one@iu.edu">one@iu.edu</a> </p>
+        </div>
+        
+        <div class="modal-footer demojs-view-policy" style=""> <a href="#" class="btn btn-outline-primary" id="returnToReport">Back</a> </div>
         
         
         
-        
-        
-     
+    </div>
 </div>
