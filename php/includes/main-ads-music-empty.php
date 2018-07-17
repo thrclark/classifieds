4bbca29dd147a9ca9ed380c1b372d0d426a9f05d
eecox <eecox@iu.edu>
2018-07-17 13:52:17 -0400

    text update

--- php/includes/main-ads-music-empty.php
+++ php/includes/main-ads-music-empty.php
@@ -2,7 +2,7 @@
 <div class="row">
     <div class="col-12">
     	<div class="alert alert-info fade show rbt-alert-list" role="alert">
-    		<p>No ads were found for 'Musicians & artists'.</p>
+    		<p>No ads found.</p>
 		</div>
     </div>
 
