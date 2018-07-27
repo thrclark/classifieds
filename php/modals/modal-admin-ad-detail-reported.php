49131d3c90df456f2ebb8f3733540dbe72f3f18a
Tom Clark <thrclark@iu.edu>
2018-07-26 23:00:33 -0400

    CCF-683

--- php/modals/modal-admin-ad-detail-reported.php
+++ php/modals/modal-admin-ad-detail-reported.php
@@ -2,14 +2,70 @@
 <div class="modal post-detail fade" id="admin_ad_detail_reported" tabindex="-1" role="dialog" aria-labelledby="admin_ad_detail_reported" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="ccf-adreport demojs-admin-defaultview" >
+            <div class="modal-header demojs-admin-defaultview" >
+                <h2 class="modal-title" id="exampleModalLabel">Math-M211 and M212 Textbook</h2>
+                <button type="button" class="close" aria-label="Close" data-dismiss="modal"> <span class="rbt-icon-close"></span> </button>
+            </div>
+           
+            <div class="modal-body demojs-admin-defaultview" >
+                <dl class="row">
+                    <dt class="sr-only">
+                        <message key="global.ad.field.price">Price</message>
+                    </dt>
+                    <dd class="col-12 ccf-postdetail-title">$20</dd>
+                    <dt class="sr-only">
+                        <message key="global.ad.field.postDate">Post Date</message>
+                    </dt>
+                    <dd class="col-auto"><span class="font-italic small">2/14/18</span></dd>
+                    <dt class="sr-only">Campus</dt>
+                    <dd class="col-auto small ccf-postdetail-market"><i class="fa fa-map-marker"></i> IU Kokomo</dd>
+                    <dt class="sr-only">
+                        <message key="global.ad.field.image">Images</message>
+                    </dt>
+                    <dd class="col-12">
+                        <ccf-image-slider>
+                            <div class="overflow mb-3" hammer-options="{&quot;touchAction&quot;: &quot;pan-y&quot;}" style="touch-action: pan-y; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                                <div class="inner" style="width: 200%; margin-left: 0%;"> <img alt="" src="../img/post-pics/mathbook.jpg" style="width: 50%;"><img alt="" src="../img/post-pics/1105499015MarkVITen5.jpg" style="width: 50%;"> </div>
+                            </div>
+                            <div class="controls">
+                                <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-left"></i> <span class="sr-only">
+                                <message key="main.imageSlider.prev">Previous image</message>
+                                </span> </button>
+                                <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-right"></i> <span class="sr-only">
+                                <message key="main.imageSlider.next">Next image</message>
+                                </span> </button>
+                            </div>
+                        </ccf-image-slider>
+                    </dd>
+                    <dt class="sr-only">
+                        <message key="global.ad.field.description">Description</message>
+                    </dt>
+                    <dd class="col-12 pre-wrap">Used textbook for Math 112. Great condition - come and get it. Auctor Platea varius volutpat. Ac vivamus lobortis natoque tincidunt aptent aliquet phasellus felis. Cubilia natoque luctus donec proin vehicula.</dd>
+                    <dt class="col-sm-3">
+                        <message key="global.ad.field.contact">Contact</message>
+                    </dt>
+                    <dd class="col-sm-9">812-555-4567</dd>
+                    <dt class="col-sm-3">
+                        <message key="global.ad.field.email">Email</message>
+                    </dt>
+                    <dd class="col-sm-9"><a href="mailto:thrclark@iu.edu">jlancer@iu.edu</a></dd>
+                    <dt class="col-sm-3">Status</dt>
+                    <dd class="col-sm-9">Active</dd>
+                    <dt class="col-sm-3">Ad Purpose</dt>
+                    <dd class="col-sm-9">For sale</dd>
+                    <dt class="col-sm-3">Posted</dt>
+                    <dd class="col-sm-9">02/25/2018 3:26pm</dd>
+                    <dt class="col-sm-3">Expires</dt>
+                    <dd class="col-sm-9">03/25/2018 3:26pm</dd>
+                </dl>
+            </div> <div class="ccf-adreport demojs-admin-defaultview" >
                 <div class="alert alert-danger fade show mb-0 border-0 pt-2 pb-2" role="alert">
                     <div class="row align-items-center">
                         <div class="col">
                             <div class="alert-heading">Reported for policy violation</div>
                         </div>
                         <div class="col-auto">
-                            <button class="btn btn-sm btn-danger demojs-btn-viewreport"> View report</button>
+                            <button class="btn btn-sm btn-link demojs-btn-viewreport"> View report</button>
                         </div>
                     </div>
                 </div>
@@ -62,71 +118,13 @@
                 <div class="card bg-white mb-1 demojs-admin-viewreport" style="display:none">
                     <div class="card-body pt-2 pb-2">
                         <div class="row">
-                            <div class="col"> <span class="card-text demojs-restoreprevreports"> <a href="#" class="">Show dismissed reports (2)</a></span></div>
+                            <div class="col"> <span class="card-text demojs-restoreprevreports"> <a href="#" class="">Show past reports (2)</a></span></div>
                             <div class="col-auto"> <span class="card-text"> <a href="#" class="demojs-btn-viewpolicy">View policy</a></span></div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="modal-header demojs-admin-defaultview" >
-                <h2 class="modal-title" id="exampleModalLabel">Math-M211 and M212 Textbook</h2>
-                <button type="button" class="close" aria-label="Close" data-dismiss="modal"> <span class="rbt-icon-close"></span> </button>
-            </div>
-            <div class="modal-body demojs-admin-defaultview" >
-                <dl class="row">
-                    <dt class="sr-only">
-                        <message key="global.ad.field.price">Price</message>
-                    </dt>
-                    <dd class="col-12 ccf-postdetail-title">$20</dd>
-                    <dt class="sr-only">
-                        <message key="global.ad.field.postDate">Post Date</message>
-                    </dt>
-                    <dd class="col-auto"><span class="font-italic small">2/14/18</span></dd>
-                    <dt class="sr-only">Campus</dt>
-                    <dd class="col-auto small ccf-postdetail-market"><i class="fa fa-map-marker"></i> IU Kokomo</dd>
-                    <dt class="sr-only">
-                        <message key="global.ad.field.image">Images</message>
-                    </dt>
-                    <dd class="col-12">
-                        <ccf-image-slider>
-                            <div class="overflow mb-3" hammer-options="{&quot;touchAction&quot;: &quot;pan-y&quot;}" style="touch-action: pan-y; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
-                                <div class="inner" style="width: 200%; margin-left: 0%;"> <img alt="" src="../img/post-pics/mathbook.jpg" style="width: 50%;"><img alt="" src="../img/post-pics/1105499015MarkVITen5.jpg" style="width: 50%;"> </div>
-                            </div>
-                            <div class="controls">
-                                <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-left"></i> <span class="sr-only">
-                                <message key="main.imageSlider.prev">Previous image</message>
-                                </span> </button>
-                                <button class="btn btn-sm btn-outline-primary" type="button"> <i aria-hidden="true" class="fa fa-angle-right"></i> <span class="sr-only">
-                                <message key="main.imageSlider.next">Next image</message>
-                                </span> </button>
-                            </div>
-                        </ccf-image-slider>
-                    </dd>
-                    <dt class="sr-only">
-                        <message key="global.ad.field.description">Description</message>
-                    </dt>
-                    <dd class="col-12 pre-wrap">Used textbook for Math 112. Great condition - come and get it. Auctor Platea varius volutpat. Ac vivamus lobortis natoque tincidunt aptent aliquet phasellus felis. Cubilia natoque luctus donec proin vehicula.</dd>
-                    <dt class="col-sm-3">
-                        <message key="global.ad.field.contact">Contact</message>
-                    </dt>
-                    <dd class="col-sm-9">812-555-4567</dd>
-                    <dt class="col-sm-3">
-                        <message key="global.ad.field.email">Email</message>
-                    </dt>
-                    <dd class="col-sm-9"><a href="mailto:thrclark@iu.edu">jlancer@iu.edu</a></dd>
-                    <dt class="col-sm-3">Status</dt>
-                    <dd class="col-sm-9">Active</dd>
-                    <dt class="col-sm-3">Ad Purpose</dt>
-                    <dd class="col-sm-9">For sale</dd>
-                    <dt class="col-sm-3">Posted</dt>
-                    <dd class="col-sm-9">02/25/2018 3:26pm</dd>
-                    <dt class="col-sm-3">Expires</dt>
-                    <dd class="col-sm-9">03/25/2018 3:26pm</dd>
-                </dl>
-            </div>
-            <div class="modal-footer demojs-admin-defaultview" >
-               <a class="btn btn-danger demojs-btn-deactivatead" href="#">Deactivate for policy violation</a> <a class="btn btn-outline-primary demojs-btn-deactivatereport" href="#" data-dismiss="modal">Dismiss report</a>
-            </div>
+            <div class="modal-footer demojs-admin-defaultview" > <a class="btn btn-danger demojs-btn-deactivatead" href="#">Deactivate ad...</a> <a class="btn btn-outline-primary demojs-btn-deactivatereport" href="#" data-dismiss="modal">Dismiss report</a> </div>
             
             <!--View policy-->
             
@@ -152,15 +150,15 @@
                 <button type="button" class="close" aria-label="Close" data-dismiss="modal"> <span class="rbt-icon-close"></span> </button>
             </div>
             <div class="modal-body demojs-admin-confirm-adremove" style="display:none">
-                <div class="font-weight-bold mb-3 mt-3"> Would you like to take administrative action on the user who posted this ad (jlacner)? </div>
-                <div class="form-check demojs-adminactiontoggle">
+                <div class="font-weight-bold mb-3 mt-3"> Does this ad violate policy? </div>
+                 <div class="form-check demojs-adminactiontoggle">
+                    <input class="form-check-input" type="radio" name="takeactiontoggle1" id="takeadminaction1" value="3">
+                    <label class="form-check-label" for="takeadminaction1"> Yes, deactivate ad and take administrative actions. </label>
+                </div><div class="form-check demojs-adminactiontoggle">
                     <input class="form-check-input" type="radio" name="takeactiontoggle1" id="watch-me3" value="2">
                     <label class="form-check-label" for="watch-me3"> No, just deactivate this ad. </label>
                 </div>
-                <div class="form-check demojs-adminactiontoggle">
-                    <input class="form-check-input" type="radio" name="takeactiontoggle1" id="takeadminaction1" value="3">
-                    <label class="form-check-label" for="takeadminaction1"> Yes </label>
-                </div>
+               
                 <div id="showadminaction1" style="display:none" class="desc">
                     <form class="">
                         <fieldset >
@@ -189,7 +187,7 @@
                         <fieldset>
                             <legend class="pt-3">Administrative</legend>
                             <div class="font-weight-bold mb-1 mt-3">Deactivate other active ads by 'jlancer'</div>
-                           <!-- <div class="form-check">
+                            <!-- <div class="form-check">
                                 <input class="form-check-input" type="checkbox" value="" id="otherad00" checked disabled>
                                 <label class="form-check-label" for="otherad00"> Math-M211 and M212 Textbook </label>
                             </div>-->
@@ -204,7 +202,7 @@
                             <div class="font-weight-bold mb-1 mt-3">Administrative action to be taken on user 'jlancer' <a href="#" class="small font-italic" data-toggle="popover" data-placement="bottom" title="Infractions for 'jlancer'" data-content="<div class='border-bottom'>Fraudulent activity</div><ul class='small list-unstyled mb-2'><li>06/26/2017</li><li>09/06/2017</li></ul><div class='border-bottom'>Selling IU parking permit</div><ul class='small list-unstyled mb-2'><li>07/26/2017</li></ul>">(3 previous infractions)</a></div>
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" name="exampleRadios22" id="stacked4" value="option1">
-                                <label class="form-check-label" for="stacked4">No action at this time. </label>
+                                <label class="form-check-label" for="stacked4">Do not log any infractions. </label>
                             </div>
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" name="exampleRadios22" id="stacked5" value="option1">
