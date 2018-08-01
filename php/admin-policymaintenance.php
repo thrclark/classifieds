a5d9e8d6000fa271acb52ba0ae16bd623193bfb4
Tom Clark <thrclark@iu.edu>
2018-08-01 14:49:33 -0400

    CCF-713

--- php/admin-policymaintenance.php
+++ php/admin-policymaintenance.php
@@ -22,7 +22,26 @@ $page_title = 'Policy maintenance';
     <div class="row">
         <div class="col-12">
             <div class="main-content" id="main-content">
-                <div class="form-group">
+             
+                
+                
+                
+                
+                
+                
+<ul class="nav nav-pills" id="pills-tab" role="tablist">
+    <li class="nav-item">
+        <button class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Policy</button>
+    </li>
+    <li class="nav-item">
+        <button class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Policy violations</button>
+    </li>
+   
+</ul>
+<div class="tab-content" id="pills-tabContent" tabindex="0" role="tabpanel">
+    <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+        <h2 class="rbt-ts-23 font-weight-bold mb-3 sr-only">Policy</h2>
+           <div class="form-group">
                     <label class="control-label" id="urlLabel">
                         <message key="policy">Policy</message>
                     </label>
@@ -30,7 +49,20 @@ $page_title = 'Policy maintenance';
                         <message key="admin.ldap.field.url.directions">Enter a policy statement which all users will agree to before posting ads.</message>
                     </div>
                     <textarea id="richText1"></textarea>
+                    
+                    
+                    
+                    
                 </div>
+    </div>
+    <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+        <h3 class="rbt-ts-23 font-weight-bold mb-3">Panel 2</h3>
+        <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+    </div>
+   
+</div>
+
+
             </div>
         </div>
     </div>
