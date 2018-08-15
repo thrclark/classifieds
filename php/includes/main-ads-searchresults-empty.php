1c0a27fa4af32bd8c6ccfeb773c844bc17d9d46b
eecox <eecox@iu.edu>
2018-08-15 14:42:28 -0400

    removed search

---
+++ php/includes/main-ads-searchresults-empty.php
@@ -0,0 +1,141 @@
+
+<div class="row">
+    <div class="col-12">
+    	<div class="alert alert-info fade show rbt-alert-list" role="alert">
+    		<p>No ads found.</p>
+		</div>
+    </div>
+
+    <div class="col-12 my-3">
+        <h2>Ads from other campuses</h2>
+    </div>
+
+    <div class="postitem grid"> <a class="postitem-container" data-toggle="modal" data-target="#post_detail" href="#">
+            <div class="postitem-row">
+                <div class="postitem-image-container">
+                    <div class="post-image" style="background-image:url(../img/post-pics/iphone4.jpg)"> </div>
+                </div>
+                <div class="postitem-post-title" >Trade ONLY: iPhone 7 Plus 256gb Jet Black Verizon </div>
+                <div class="postitem-price" >$0</div>
+                <div class="w-100"></div>
+                <div class="postitem-description" >Both phones are unlocked and ready to use we upgraded so no longer need both phones work great no problems but both screens are cracked but don't effect the phones at all have cases for them if wanted as well </div>
+                <div class="postitem-postdate" >Oct 16</div>
+                <div class="postitem-postlocation" ><i class="fa fa-map-marker" aria-hidden="true"></i> IUB</div>
+            </div>
+        </a>
+        <div class="postitem-functions">
+            <div class="postitem-functions-set">
+                <button class="btn btn-link favorite" type="button"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="sr-only">
+                <message key="main.watchlist.add">Save ad to watchlist</message>
+                </span> </button>
+            </div>
+        </div>
+    </div>
+    <div class="postitem grid"> <a class="postitem-container" data-toggle="modal" data-target="#post_detail" href="#">
+            <div class="postitem-row">
+                <div class="postitem-image-container">
+                    <div class="post-image" style="background-image:url(../img/post-pics/iphone5.jpg)"> </div>
+                </div>
+                <div class="postitem-post-title" >For Sale:  Gold iPhone 8 plus 64GB </div>
+                <div class="postitem-price" >$550</div>
+                <div class="w-100"></div>
+                <div class="postitem-description" >Can meet you at Verizon store. Clean esn. Excellent condition with AppleCare+ until Sept 2018. See pictures! Phone only. </div>
+                <div class="postitem-postdate" >Oct 16</div>
+                <div class="postitem-postlocation" ><i class="fa fa-map-marker" aria-hidden="true"></i> IUB</div>
+            </div>
+        </a>
+        <div class="postitem-functions">
+            <div class="postitem-functions-set">
+                <button class="btn btn-link favorite" type="button"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="sr-only">
+                <message key="main.watchlist.add">Save ad to watchlist</message>
+                </span> </button>
+            </div>
+        </div>
+    </div>
+    <div class="postitem grid"> <a class="postitem-container" data-toggle="modal" data-target="#post_detail" href="#">
+            <div class="postitem-row">
+                <div class="postitem-image-container">
+                    <div class="post-image" style="background-image:url(../img/post-pics/iphone6.jpg)"> </div>
+                </div>
+                <div class="postitem-post-title" >For Sale: iPhone 6s </div>
+                <div class="postitem-price" >$150</div>
+                <div class="w-100"></div>
+                <div class="postitem-description" >I have approximately 100 new sets of photojojo 3 lense sets. The product has not been used but packages have been open to verify contents. $10 each if you buy more than one set price can fluctuate. These are selling for $70+ </div>
+                <div class="postitem-postdate" >Oct 16</div>
+                <div class="postitem-postlocation" ><i class="fa fa-map-marker" aria-hidden="true"></i> IUPUI</div>
+            </div>
+        </a>
+        <div class="postitem-functions">
+            <div class="postitem-functions-set">
+                <button class="btn btn-link favorite" type="button"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="sr-only">
+                <message key="main.watchlist.add">Save ad to watchlist</message>
+                </span> </button>
+            </div>
+        </div>
+    </div>
+    <div class="postitem grid"> <a class="postitem-container" data-toggle="modal" data-target="#post_detail" href="#">
+            <div class="postitem-row">
+                <div class="postitem-image-container">
+                    <div class="post-image" style="background-image:url(../img/post-pics/iphone7.jpg)"> </div>
+                </div>
+                <div class="postitem-post-title" >For Sale: iPhone SE Rose Gold 16gb ATT or Cricket </div>
+                <div class="postitem-price" >$225</div>
+                <div class="w-100"></div>
+                <div class="postitem-description" >I have for sale an iPhone SE Rose Gold 16gb ATT or Cricket </div>
+                <div class="postitem-postdate" >Oct 16</div>
+                <div class="postitem-postlocation" ><i class="fa fa-map-marker" aria-hidden="true"></i> IUPUI</div>
+            </div>
+        </a>
+        <div class="postitem-functions">
+            <div class="postitem-functions-set">
+                <button class="btn btn-link favorite" type="button"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="sr-only">
+                <message key="main.watchlist.add">Save ad to watchlist</message>
+                </span> </button>
+            </div>
+        </div>
+    </div>
+    <div class="postitem grid"> <a class="postitem-container" data-toggle="modal" data-target="#post_detail" href="#">
+            <div class="postitem-row">
+                <div class="postitem-image-container">
+                    <div class="post-image" style="background-image:url(../img/post-pics/iphone8.jpg)"> </div>
+                </div>
+                <div class="postitem-post-title" >For Sale: IPhone 6/6s casebrite cover </div>
+                <div class="postitem-price" >$30</div>
+                <div class="w-100"></div>
+                <div class="postitem-description" >Brand new iPhone 7 not even 1month old. Comes with box it came in as well as charger. Carrier is verizon. Has a good esn as well.im flexible on price. </div>
+                <div class="postitem-postdate" >Oct 16</div>
+                <div class="postitem-postlocation" ><i class="fa fa-map-marker" aria-hidden="true"></i> IUPUI</div>
+            </div>
+        </a>
+        <div class="postitem-functions">
+            <div class="postitem-functions-set">
+                <button class="btn btn-link favorite" type="button"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="sr-only">
+                <message key="main.watchlist.add">Save ad to watchlist</message>
+                </span> </button>
+            </div>
+        </div>
+    </div>
+    <div class="postitem grid"> <a class="postitem-container" data-toggle="modal" data-target="#post_detail" href="#">
+            <div class="postitem-row">
+                <div class="postitem-image-container">
+                    <div class="post-image" style="background-image:url(../img/post-pics/iphone9.jpg)"> </div>
+                </div>
+                <div class="postitem-post-title" >For Sale: iPhone 7Plus 256gb Jet Black </div>
+                <div class="postitem-price" >$700</div>
+                <div class="w-100"></div>
+                <div class="postitem-description" >Interest or question shoot me email. Serious buyer and CASH only. MUST PICK UP, no hold, Thanks.
+                    
+                    This device is in Mint condition, Verizon unlocked will work with any carrier. Paid close to $1000 asking $700 and I might take $20-25 less than asking price, not penny more. Come case orginal box, charger and cable. </div>
+                <div class="postitem-postdate" >Oct 16</div>
+                <div class="postitem-postlocation" ><i class="fa fa-map-marker" aria-hidden="true"></i> IUPUI</div>
+            </div>
+        </a>
+        <div class="postitem-functions">
+            <div class="postitem-functions-set">
+                <button class="btn btn-link favorite" type="button"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="sr-only">
+                <message key="main.watchlist.add">Save ad to watchlist</message>
+                </span> </button>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ php/main-searchresults-empty.php
@@ -0,0 +1,55 @@
+<?php 
+$audience = 'main';
+$page_title = 'Results for';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include('includes/all-head-meta.php') ?>
+<?php include('includes/main-styles.php') ?>
+</head>
+<body>
+<div id="main">
+    <?php include('includes/main-appheader.php') ?>
+    <section class="main-content pt-3" role="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-3 d-none d-md-block nav-section">
+                    <?php include('includes/main-nav.php') ?>
+                </div>
+                <div class="col-xs-12 col-md-9 main-content-section" id="main-conten">
+                    <div class="row align-items-center section-head">
+                        <div class="col-12 section-head-title">
+                            <h1 class="mb-2">
+                                <?php echo $page_title; ?> 'iPhone'  <button onclick="location.href='main-home.php';" class="btn btn-link ccf-clearsearch">(clear)</button>
+                            </h1>
+                        </div>
+                        <div class="col-12">
+                            <?php include('includes/main-section-utilities.php') ?>
+                        </div>
+                    </div>
+                    <?php include('includes/main-ads-searchresults-empty.php') ?>
+                    <?php include('includes/all-pagination.php') ?>
+                </div>
+            </div>
+        </div>
+        <?php include('modals/modal-main-ad-detail.php') ?>
+    </section>
+    <?php include('includes/main-footer-menu.php') ?>
+</div>
+<?php include('includes/admin-footerscripts.php') ?>
+<script>
+    $(document).ready(function() {
+        if (screen.width < 767) {
+            $(".rbt-brand-image-container, .rbt-brand-title-container, #mobile_showSearch, .rbt-drawer-button").hide();
+			$("#mobile_hideSearch, #cleartext2").show();
+			$("#mainSearch_container").removeClass( "d-none d-md-block" );
+			$("#mainSearch_container").addClass( "pr-0" );
+			 $("#mainSearch").attr("value", "iPhone");
+        } else {
+			$("#mainSearch").attr("value", "iPhone");
+        }
+    });
+</script>
+</body>
+</html>
