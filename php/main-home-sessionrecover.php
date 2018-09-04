3cd245824d8c78414def49b8f18417e9efc820ad
Tom Clark <thrclark@iu.edu>
2018-09-04 12:28:44 -0700

    CCF-737

---
+++ php/main-home-sessionrecover.php
@@ -0,0 +1,59 @@
+<?php 
+$audience = 'main';
+$page_title = 'All ads';	
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php include('includes/all-head-meta.php') ?>
+<?php include('includes/main-styles.php') ?>
+</head>
+<body>
+<div id="main">
+    <?php include('includes/main-appheader.php') ?>
+    <section class="main-content pt-3" role="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-3 d-none d-md-block nav-section">
+                    <?php include('includes/main-nav.php') ?>
+                </div>
+                <div class="col-xs-12 col-md-9 main-content-section" role="main">
+                    <div class="row align-items-center section-head">
+                        <div class="col-12 section-head-title">
+                            <h1 class="mb-2"> <?php echo $page_title; ?></h1>
+                        </div>
+                        <div class="col-12">
+                            <?php include('includes/main-section-utilities.php') ?>
+                        </div>
+                    </div>
+                    <?php include('includes/main-ads-all.php') ?>
+                    <?php include('includes/all-pagination.php') ?>
+                </div>
+            </div>
+        </div>
+        <?php include('modals/modal-main-ad-detail.php') ?>
+        <?php include('modals/modal-main-ad-detail-reported.php') ?>
+        <?php include('modals/modal-main-sessionrecover.php') ?>
+    </section>
+    <?php include('includes/main-footer-menu.php') ?>
+</div>
+<?php include('includes/admin-footerscripts.php') ?>
+
+
+
+
+<script>
+    $(document).ready(function() {
+        setTimeout(function() {
+            $('#session_recover').modal();
+        }, 2000);
+    });
+	$('#refreshsession').click(function() {
+    location.reload();
+});
+</script>
+
+
+
+</body>
+</html>
