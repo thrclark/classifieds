b4b57b5411b379e06c8c42befda7a206d4a55cb0
Tom Clark <thrclark@iu.edu>
2018-09-13 11:26:40 -0400

    CCF-516

--- php/includes/all-customheader.php
+++
@@ -1,7 +0,0 @@
-<section style="background:red;">
-    <div class="container">
-        <div class="row">
-            <div class="col"><p class="pt-3 pb-3" style="color:white">Custom Header Custom Header Custom Header Custom Header Custom Header Custom Header Custom Header Custom Header Custom Header Custom Header Custom Header Custom Header Custom Header Custom Header Custom Header Custom Header Custom Header </p></div>
-        </div>
-    </div>
-</section>
--- php/modals/modal-main-sessionrecover.php
+++ php/modals/modal-main-sessionrecover.php
@@ -5,7 +5,7 @@
             <div class="modal-header">
                 <h2 class="modal-title" id="exampleModalLabel">Session expired</h2>
             </div>
-            <div class="modal-body"> Your session has expired due to inactivity. Try refreshing this page to renew your session. </div>
+            <div class="modal-body"> Your session has expired due to inactivity. Refresh this page to renew your session. </div>
             <div class="modal-footer">
                 <div class="rbt-button-group">
                     <button class="btn btn-outline-primary" type="button" id="refreshsession"> Refresh </button>
