29b7e8eedac41e1bf4c46e7b7ab2d28702d0d33d
eecox <eecox@iu.edu>
2020-05-27 21:48:17 -0400

    CCF-804

---
+++ php/includes/main-ads-user.php
@@ -0,0 +1,117 @@
+<div class="row">
+    <div class="postitem grid"> <a class="postitem-container" data-toggle="modal" data-target="#post_detail" href="#">
+            <div class="postitem-row">
+                <div class="postitem-image-container">
+                    <div class="post-image none"> <i aria-hidden="true" class="fa fa-asterisk"></i> <span class="sr-only">asdf</span> </div>
+                </div>
+                <!-- <div class="postitem-newbanner" >New</div>-->
+                <div class="postitem-post-title" >For Sale: Subleasing Townhouse </div>
+                <div class="postitem-price" >$455/month</div>
+                <div class="w-100"></div>
+                <div class="postitem-description" >Looking for someone to take my room in our 3 bed, 3.5 bath townhouse. Spacious living room and kitchen. Each room has its own full bathroom, and the laundry room is on the first floor inside the kitchen. My roommates are both athletes here at IU, and are great guys. I was medically DQ'd so unfortunately I have to transfer at semester. </div>
+                <div class="postitem-postdate" >Oct 16</div>
+                <div class="postitem-postlocation" ><i class="fa fa-map-marker" aria-hidden="true"></i> IUK</div>
+            </div>
+        </a>
+        <div class="postitem-functions">
+            <div class="postitem-functions-set">
+                <button class="btn btn-link favorite" type="button"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="sr-only">
+                <message key="main.watchlist.add">Save ad to watchlist</message>
+                </span> </button>
+            </div>
+        </div>
+    </div>
+    <div class="postitem grid"> <a class="postitem-container" data-toggle="modal" data-target="#post_detail_reported" href="#">
+            <div class="postitem-row">
+                <div class="postitem-image-container">
+                    <div class="post-image none"> <i aria-hidden="true" class="fa fa-asterisk"></i> <span class="sr-only">asdf</span> </div>
+                </div>
+                <!-- <div class="postitem-newbanner" >New</div>-->
+                <div class="postitem-post-title" >Seeking: Looking for roommate! </div>
+                <div class="postitem-price" >$330/month</div>
+                <div class="w-100"></div>
+                <div class="postitem-description" >Looking for a guy or girl that's super chill to live with me & my two other roommates! We have two dogs and we love to have fun! We live at The Quarry on Copper Beech! Let me know ASAP!!!! </div>
+                <div class="postitem-postdate" >Oct 16</div>
+                <div class="postitem-postlocation" ><i class="fa fa-map-marker" aria-hidden="true"></i> IUK</div>
+            </div>
+        </a>
+        <div class="postitem-functions">
+            <div class="postitem-functions-set">
+                <button class="btn btn-link favorite" type="button"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="sr-only">
+                <message key="main.watchlist.add">Save ad to watchlist</message>
+                </span> </button>
+            </div>
+        </div>
+    </div>
+    <div class="postitem grid"> <a class="postitem-container" data-toggle="modal" data-target="#post_detail" href="#">
+            <div class="postitem-row">
+                <div class="postitem-image-container">
+                    <div class="post-image" style="background-image:url(../img/post-pics/ride1.jpg)"> </div>
+                </div>
+                <!-- <div class="postitem-newbanner" >New</div>-->
+                <div class="postitem-post-title" >Oct 6. Ride to Northwest Indiana </div>
+                <div class="postitem-price" >$25</div>
+                <div class="w-100"></div>
+                <div class="postitem-description" >My girlfriend and I will be leaving to The Region on Friday October 6. We will be leaving at 11:00am or 12:00pm on Friday and we only have 2 seats left out of 3. Text me if you are interested or have any questions and I will get back to you. </div>
+                <div class="postitem-postdate" >Oct 16</div>
+                <div class="postitem-postlocation" ><i class="fa fa-map-marker" aria-hidden="true"></i> IUK</div>
+            </div>
+        </a>
+        <div class="postitem-functions">
+            <div class="postitem-functions-set">
+                <button class="btn btn-link favorite" type="button"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="sr-only">
+                <message key="main.watchlist.add">Save ad to watchlist</message>
+                </span> </button>
+            </div>
+        </div>
+    </div>
+    <div class="postitem grid"> <a class="postitem-container" data-toggle="modal" data-target="#post_detail" href="#">
+            <div class="postitem-row">
+                <div class="postitem-image-container">
+                    <div class="post-image" style="background-image:url(../img/post-pics/furniture1.jpg)"> </div>
+                </div>
+                <!-- <div class="postitem-newbanner" >New</div>-->
+                <div class="postitem-post-title" >For Sale: Crate and Barrel wood folding chair </div>
+                <div class="postitem-price" >$80</div>
+                <div class="w-100"></div>
+                <div class="postitem-description" >Two Crate and Barrel spare folding chairs made from solid wood. Chairs are in excellent condition and match my other ad on here for C&B Vintner Dining Chairs. Comes in dove (creamy off-white/grey color). Chairs retail for $50, selling both for $80 OBO. See more information at C&B: </div>
+                <div class="postitem-postdate" >Oct 16</div>
+                <div class="postitem-postlocation" ><i class="fa fa-map-marker" aria-hidden="true"></i> IUK</div>
+            </div>
+        </a>
+        <div class="postitem-functions">
+            <div class="postitem-functions-set">
+                <button class="btn btn-link favorite" type="button"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="sr-only">
+                <message key="main.watchlist.add">Save ad to watchlist</message>
+                </span> </button>
+            </div>
+        </div>
+    </div>
+    <div class="postitem grid"> <a class="postitem-container" data-toggle="modal" data-target="#post_detail" href="#">
+            <div class="postitem-row">
+                <div class="postitem-image-container">
+                    <div class="post-image none"> <i aria-hidden="true" class="fa fa-asterisk"></i> <span class="sr-only">asdf</span> </div>
+                </div>
+                <!-- <div class="postitem-newbanner" >New</div>-->
+                <div class="postitem-post-title" >Downsizing Sale - Games/Electronics/Bike Basket/Home </div>
+                <div class="postitem-price" >Varies</div>
+                <div class="w-100"></div>
+                <div class="postitem-description" >Doing some downsizing and have a few things I'd like to get rid of. Please click here to view items: https://djhumphries.files.wordpress.com/2017/10/classifieds-listing.pdf
+                    
+                    Includes some gaming systems, games and accessories, bicycle basket, bluetooth earpiece, spotlifter, diffuser, cat box, etc.
+                    
+                    I live on the south side of town and don't bring all of these with me every day so I do need warning to meet up on Bloomington campus. Cash or Googlewallet only. Let me know if you have questions! </div>
+                <div class="postitem-postdate" >Oct 16</div>
+                <div class="postitem-postlocation" ><i class="fa fa-map-marker" aria-hidden="true"></i> IUK</div>
+            </div>
+        </a>
+        <div class="postitem-functions">
+            <div class="postitem-functions-set">
+                <button class="btn btn-link favorite" type="button"> <i aria-hidden="true" class="fa fa-star-o"></i> <span class="sr-only">
+                <message key="main.watchlist.add">Save ad to watchlist</message>
+                </span> </button>
+            </div>
+        </div>
+    </div>
+
+</div>
