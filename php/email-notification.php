d95991b21847408e084f5693a620a75edaab422a
eecox <eecox@iu.edu>
2020-07-27 13:34:36 -0400

    CCF-799

--- php/email-notification.php
+++ php/email-notification.php
@@ -7,7 +7,6 @@ $page_title = 'Newest 6 items this week';
 <head>
 <?php include('includes/all-head-meta.php') ?>
 <?php include('includes/main-styles.php') ?>
-<?php include('../../../GitHub-Notifications/php/includes/nc-styles.php') ?>
 <style>
 body .nav-section {
     border-right: none !important;
@@ -54,40 +53,201 @@ body .nav-section {
 .postitem .postitem-container .postitem-post-title::before {
     background: none !important;
 }
+.viewonline {
+	padding: 16px 18px; margin: 0px; color: #990000; font-family: Arial,Helvetica,sans-serif; font-size: 10px; line-height: 17px;
+}
+.viewonline a {
+	color: #990000; text-decoration: none;
+}
+.sourcetext {
+	padding: 50px 18px 0px 18px; 
+	margin: 0px; 
+	font-family: Arial,Helvetica,sans-serif; 
+	font-size: 14px; 
+	font-weight: normal; 
+	font-style: normal; 
+	color: #4d5457;
+}
+.headingtext {
+	padding: 12px 18px 0px; 
+	margin: 0px; 
+	font-size: 32px; 
+	font-weight: bold; 
+	font-style: normal; 
+	line-height: 36px;
+}
+.subjecttext {
+	padding: 12px 18px 0px; 
+	margin: 0px; 
+	font-family: Arial,Helvetica,sans-serif; 
+	font-size: 18px; 
+	font-weight: normal; 
+	font-style: normal; 
+	color: #4d5457;
+}
+.emailtext {
+	padding: 50px 10px 4px; 
+	margin: 0px; 
+	font-style: normal; 
+	font-family: Arial,Helvetica,sans-serif; 
+	font-size: 10px; 
+	font-weight: normal; 
+	line-height: 17px; 
+	color: #8f8f8f;
+}
+.comstext {
+	padding: 0px 10px 0px; 
+	margin: 0px; 
+	font-style: normal; 
+	font-family: Arial,Helvetica,sans-serif; 
+	font-size: 10px; 
+	font-weight: normal; 
+	line-height: 17px; 
+	color: #8f8f8f;
+}
+ul {
+  list-style-type: none;
+}
+
+ul li:before {
+  content: '\2014';
+  position: absolute;
+  margin-left: -25px;
+}
 </style>
 </head>
 <body>
+<div>
+<table  width="100%" bgcolor="" border="0"  cellpadding="0" cellspacing="0">
+		<tr>
+        	<td style="font-family:Arial; font-size:13px">
+        		<table width="100%" cellspacing="0" cellpadding="0" border="0" align="center" style="margin: 0px; padding: 0px; border-collapse: collapse;">
+                    <tbody>
+                    <tr>
+						<td align="center" colspan="3"><h1>HTML default email</h1></td>
+					</tr>
+                    <tr>
+                        <td valign="top" align="left" style="padding: 0px; margin: 0px;">&nbsp;</td>
+                        <td width="464" valign="top" align="center" class="viewonline"><a target="_blank" href="#">View online.</a></td>
+                        <td valign="top" align="left" style="padding: 0px; margin: 0px;">&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td valign="top" align="left" style="padding:0; margin:0;">&#160;</td>
+                        <td width="600" valign="top" align="left" style="padding:0; margin:0; background-color:#ffffff;">
+                        	<table cellpadding="0" cellspacing="0" border="0"  width="100%" bgcolor="">
+                    			<tr>
+                                    <td>
+                                    	<table  width="100%" bgcolor="" border="0"  cellpadding="0" cellspacing="0">
+                                            <tr>
+                                            	<td style="font-family:Arial; font-size:13px"><div align="center"> <a href="http://www.iu.edu/" title="Go to www.iu.edu"  ><img width="600" border="0" height="60" style="width: 600px; height: 63px; border-color: #000000; margin: 0px;" alt="IU Campus Branding Bar"  src="http://lesc.iu.edu/emails/_____templates/_common/branding/600px/mcv2/evite-banner-iu-600x63px.png" title="IU Bloomington Branding Bar"/></a> </div></td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td valign="top" align="left" style="padding:0; margin:0;">&#160;</td>
+                    </tr>
+                    <tr>
+                        <td valign="top" align="left" style="padding:0; margin:0;">&#160;</td>
+                        <td width="600" valign="top" align="center" style="padding:0; margin:0; background-color:#ffffff;"><table cellpadding="0" cellspacing="0" border="0"  width="100%" bgcolor="">
+                    <tr>
+                        <td style="padding: 0px 0px;">
+                        	<table width="100%" cellspacing="0" cellpadding="0" border="0">
+                                <tbody>
+                                <tr>
+                                    <td valign="top" class="sourcetext"> IU Classifieds </td>
+                                <tr>
+                                    <td valign="top" class="headingtext">
+                                    	<h1 class="">New matches for "car"</h1>
+                                    	<h2 class="rbt-ts-20 mb-1">Newest 6 items today</h2>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td valign="top" class="subjecttext">
+                                    	<ul class="mb-3">
+                        		<li>For Sale: 1995 Honda Accord LX 4-dr Sedan, $4,995 at IUK</li>
+                        		<li>For Sale: Honda Accord 2001 (150K miles), $2,950 at IUK</li>
+                        		<li>For Sale: Rent my car, $9,995 at IUK</li>
+                        		<li>For Sale: 1995 Invisible car, $73,995 at IUK</li>
+                        		<li>For Sale: 1995 Honda Accord LX 4-dr Sedan, $4,995 at IUK</li>
+                        		<li>For Sale: Honda Accord 2001 (150K miles), $2,950 at IUK</li>
+                        	</ul>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td valign="top" style="padding: 12px 18px 30px 18px; margin: 0px;">
+                                    	<div class="rbt-button-group">
+                                        	<button class="btn btn-primary">Primary action</button>
+                                            <button class="btn btn-outline-primary">Secondary action</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                	</tbody>
+                </table>
+            </td>
+            <td valign="top" align="left" style="padding:0; margin:0;">&#160;</td>
+        </tr>
+    </table>
+    <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center" style="margin: 0px; padding: 0px; border-collapse: collapse;">
+        <tbody>
+        <tr>
+            <td valign="top" align="left" style="padding:0; margin:0;">&#160;</td>
+            <td width="600" valign="top" align="left" style="padding:30px 0px 0px 0px; margin:0; background-color:#ffffff;">
+            	<table cellpadding="0" cellspacing="0" border="0"  width="100%" bgcolor="">
+        			<tr>
+                        <td valign="top" align="center">
+                        	<table width="100%" bgcolor="#950d00" border="0"  cellpadding="0" cellspacing="0">
+                                <tr>
+                                	<td style="font-family:Arial; font-size:13px"><img width="600" border="0" style="border-color: #000000; margin: 0px; display:block" alt="IU Campus Branding Bar"  src="http://lesc.iu.edu/emails/_____templates/_common/footer/fulfilling-the-promise-600x40.png" title="Promise Footer"/></td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+            <td valign="top" align="left" style="padding:0; margin:0;">&#160;</td>
+        </tr>
+        </tbody>
+    </table>
+    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+            <td align="center" class="emailtext">This e-mail was sent to Eric Cox (<a href="#">eecox@iu.edu</a>) by Indiana University. Learn why we are including this: <a href="#">go.iu.edu/emailsafety</a></td>
+        </tr>
+        <tr>
+            <td align="center" class="comstext">To update communication preferences, visit the <a href="#">Notification Center</a>.</td>
+        </tr>
+    </table>
+</div>
 
-<div id="main">
-    <section class="main-content pt-3" role="main">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-md-9 main-content-section" role="main">
-                    <!-- <h1 class="rbt-ts-20 mb-5">Email - ( from: IU Classifieds ) ( email subject: "car: 4 new!" ) </h1> -->
-                    <div class="emailHeader">       
-                    	<header class="d-flex flex-nowrap rbt-app-header mt-2">
-							<div class="ccf-brand-image-container d-none d-sm-block"><img src="../img/trident-large.png" alt="" class="rbt-brand-image"/></div>
-    						<div class="rbt-brand-title-container flex-grow-1 "> <a href="#" class="rbt-brand-title font-weight-bold ml-2">IU Classifieds </a> </div>
-						</header>
-						<div class="mt-2 mb-2"><h2 class="rbt-ts-29 mt-3 mb-3"> New matches for "car"</h2>
-							<a href="main-searchresults.php" class="btn btn-sm btn-outline-primary">See all results</a> <a href="main-savedsearch.php" class="btn btn-sm btn-outline-primary">Manage search settings</a> 
-						</div>
-					</div>
-                    <div class="row align-items-center section-head mt-3">
-                        <div class="col-12 section-head-title">
-                            <h3 class="resultTitle rbt-ts-26 mb-2"> <?php echo $page_title; ?></h3>
-                        </div>
-                        <div class="col-12">
-                            
-                        </div>
-                    </div>
-                    
-                    <?php include('includes/main-ads-email.php') ?>
-                    
-                </div>
-            </div>
-        </div>
-    </section>
+<div style=" margin-top:100px; width:600px; margin-left:auto; margin-right:auto; margin-bottom: 40px; font-size: 16px;"><h1 style="text-align: center; margin-bottom: 40px;">Plain text default email</h1>
+	NEW MATCHES FOR "CAR"<br /><br />
+    Priority: STANDARD<br />
+    From: IU Classifieds<br />
+    Delivered: Jul 23, 2020 | 1:38 PM<br />
+    <br />
+    <span class="nc-card-detail-text">
+    Newest 6 items today
+    <ul>
+        <li>For Sale: 1995 Honda Accord LX 4-dr Sedan, $4,995 at IUK</li>
+        <li>For Sale: Honda Accord 2001 (150K miles), $2,950 at IUK</li>
+        <li>For Sale: Rent my car, $9,995 at IUK</li>
+        <li>For Sale: 1995 Invisible car, $73,995 at IUK</li>
+        <li>For Sale: 1995 Honda Accord LX 4-dr Sedan, $4,995 at IUK</li>
+        <li>For Sale: Honda Accord 2001 (150K miles), $2,950 at IUK</li>
+    </ul>
+    </span>
+    <br />
+    <br />
+    See all results: <a href="#">http://link1.iu.edu/primary_action</a><br />
+    Manage search settings: <a href="#">http://link2.iu.edu/secondary_action</a><br />
+    <br />
+    This e-mail was sent to Eric Cox (<a href="#">eecox@iu.edu</a>) by Indiana University. Learn why we are including this: <a href="#">go.iu.edu/emailsafety</a>.
+    To update communication preferences, visit the <a href="#">Notification Center</a>.
 </div>
 
 </body>
